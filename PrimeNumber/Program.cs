424a9999c6bb3a86d9d16bae9c34690bd3cb9b4c
mikhalenkoilya <mihalenkoilya@gmail.com>
2024-10-07 22:44:14 +0500

    Добавьте файлы проекта.

---
+++ PrimeNumber/Program.cs
@@ -0,0 +1,45 @@
+﻿int counter = 2;
+int primeCounter = 1;
+int primeNumberCount = 0;
+
+
+primeNumberCount = Input();
+if (primeNumberCount <= 0) 
+{
+    Console.WriteLine("Введите число больше 0");
+    primeNumberCount = Input();
+}
+while (primeCounter < primeNumberCount)
+{
+    counter++;
+    if (isPrime(counter) == true)
+    {
+        primeCounter++;
+    }
+}
+Console.WriteLine($"Ваше простое число: {counter}");
+
+int Input()
+{
+    try
+    {
+        Console.Write("Введите порядковый номер: ");
+        return int.Parse(Console.ReadLine());
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("Неправильный ввод данных");
+        return Input();
+    }
+}
+bool isPrime(int number)
+{
+    for (int i = 2; i <= Math.Sqrt(number); i++)
+    {
+        if (number % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
